c126a05f70593f1a9db5c581fc4c98ba0de9cc16
Lena <lenach241@gmail.com>
2018-09-29 22:40:24 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,103 @@
 class SmartCalculator {
+
   constructor(initialValue) {
-    // your implementation
+    this.polishNotation = [];
+    this.polishNotation.push(initialValue);  
+    this.stack = [];  
   }
 
   add(number) {
-    // your implementation
+    while (this.stack.length > 0 && (this.stack[this.stack.length-1] == "-" || this.stack[this.stack.length-1] == "/"
+     || this.stack[this.stack.length-1] == "^" || this.stack[this.stack.length-1] == "*" 
+     || this.stack[this.stack.length-1] == "+"  )) {
+      this.polishNotation.push(this.stack.pop());  
+    }  
+    this.stack.push("+"); 
+    this.polishNotation.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    while (this.stack.length > 0 && (this.stack[this.stack.length-1] == "-" || this.stack[this.stack.length-1] == "/" 
+      || this.stack[this.stack.length-1] == "^" || this.stack[this.stack.length-1] == "*" 
+      || this.stack[this.stack.length-1] == "+"  )) {
+      this.polishNotation.push(this.stack.pop()); 
+    }  
+    this.stack.push("-"); 
+    this.polishNotation.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    while (this.stack.length > 0 && (this.stack[this.stack.length-1] == "/" || this.stack[this.stack.length-1] == "^" 
+      || this.stack[this.stack.length-1] == "*")) {
+      this.polishNotation.push(this.stack.pop());
+    }
+    this.stack.push("*"); 
+    this.polishNotation.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    while (this.stack.length > 0 && (this.stack[this.stack.length-1] == "*" || this.stack[this.stack.length-1] == "^" 
+      || this.stack[this.stack.length-1] == "/")) {
+      this.polishNotation.push(this.stack.pop());
+    }
+    this.stack.push("/"); 
+    this.polishNotation.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push("^"); 
+    this.polishNotation.push(number);
+    return this;
+  }
+
+  getPolishNotation() {
+    while (this.stack.length > 0) {
+      this.polishNotation.push(this.stack.pop());
+    }
+    return this.polishNotation;
+  }
+
+  valueOf() {
+    this.getPolishNotation();
+    return this.calculate();
+  }
+
+  calculate() {
+    var newStack = [];
+    for (var i = 0; i < this.polishNotation.length; i++) {
+      var nextFirstValue = this.polishNotation[i];
+      if (typeof nextFirstValue == 'number') {
+        newStack.push(nextFirstValue);
+      } 
+      else {
+        var lastNumber = newStack.pop();
+        var prevLastNumber = newStack.pop();
+            switch (nextFirstValue) {
+              case '+':
+                newStack.push(prevLastNumber + lastNumber);
+                break;
+              case '-':
+                newStack.push(prevLastNumber - lastNumber);
+                break;
+              case '*':
+                newStack.push(prevLastNumber * lastNumber);
+                break;
+              case '/':
+                newStack.push(prevLastNumber / lastNumber);
+                break;
+              case '^':
+                newStack.push(Math.pow(prevLastNumber, lastNumber));
+                break;  
+              default:
+                break;
+            }
+      }
+    }
+    return newStack.pop();
   }
 }
 
